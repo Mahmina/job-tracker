68cf8d170f804ba73efebf4327679ab3dcf14fe1
Mina Mahmoodi <min.mahmoodi89@gmail.com>
2025-10-02 11:00:29 +0200

    Extract desired information from each job, posted in the last 24h

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ chrome_options.add_experimental_option("detach", True)
 user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
 chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 driver = webdriver.Chrome(options=chrome_options)
+driver.maximize_window()
 driver.get(WEBSITE_URL)
 
-wait = WebDriverWait(driver, 2)
+wait = WebDriverWait(driver, 10)
 input_elements = wait.until(
     ec.presence_of_all_elements_located(
         (By.CSS_SELECTOR, '#app-searchBar [data-genesis-element="FORM_INPUT"]')
@@ -60,4 +61,40 @@ search_btn = wait.until(
     )
 )
 search_btn.click()
+
+last_24_jobs = wait.until(
+    ec.element_to_be_clickable(
+        (By.CSS_SELECTOR, '[data-at="desktop-filters-container"] [data-at="facet-link"]')
+    )
+)
+last_24_jobs.click()
+
+new_top_card = wait.until(
+    ec.presence_of_element_located(
+        (By.XPATH, "//*[normalize-space(text())='NEU']")
+    )
+)
+time.sleep(6)
+
+job_cards = wait.until(
+    ec.presence_of_all_elements_located(
+        (By.CSS_SELECTOR, '[data-genesis-element="CARD"][data-at="job-item"]')
+    )
+)
+
+desired_time_str = {"Stunden", "Stunde", "Minuten", "Minute"}
+for card in job_cards:
+    time_posted = card.find_element(By.CSS_SELECTOR, '[data-at="job-item-middle"] [data-at="job-item-timeago"]').text
+
+    if any(word in time_posted for word in desired_time_str):
+        job_title = card.find_element(By.XPATH, ".//h2").text
+        city = card.find_element(By.CSS_SELECTOR, '[data-at="job-item-location"]').text
+        company_name = card.find_element(By.CSS_SELECTOR, '[data-at="job-item-company-name"]').text
+        job_link = card.find_element(By.XPATH, ".//h2/a").get_attribute("href")
+
+        # print(job_title)
+        # print(city)
+        # print(company_name)
+        # print(job_link)
+        # print(time_posted)
 
